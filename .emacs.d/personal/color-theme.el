1ffd05cbe28dc77e10e958c430b23f1ad83c84ae
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-04-09 10:42:07 +0300

    add solarized theme.

---
+++ .emacs.d/personal/color-theme.el
@@ -0,0 +1,7 @@
+(prelude-require-package 'color-theme-solarized)
+
+;; from here https://github.com/sellout/emacs-color-theme-solarized
+
+;;(disable-theme 'zenburn)
+;;(setq prelude-theme 'solarized-dark)
+(load-theme 'solarized t)
